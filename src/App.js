2c09ff915a07af4d779cdce93ed20796c7fa0519
Chaowat Lee <77148658+chaowat-lee13@users.noreply.github.com>
2021-11-26 22:24:47 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I dont know how to do!
         </a>
       </header>
     </div>
